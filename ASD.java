e8edaba12f4991f3df3dbb7d784d80d31241aefd
“satyanarayana <satya_eday@yahoo.com>
2018-04-15 22:20:13 -0500

    ASD commit

---
+++ ASD.java
@@ -0,0 +1,17 @@
+
+
+public class ASD {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=10;i++)
+		{
+		                        	 
+		   	 System.out.println("hello");
+		                        	 
+		                        	 
+		 }
+
+	}
+
+}
